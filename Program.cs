1ff2608b7bf40e2984bbdf8b02b877b79d977f86
Korisnik <Korisnik@DESKTOP-DIMB8PE>
2022-04-05 12:55:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace klasaInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo(@"c:\");
+            Console.WriteLine("\n Poddirektorij: ");
+            
+            foreach(DirectoryInfo diInfo in di.GetDirectories())
+            {
+                Console.WriteLine("{0}\t{1}\t{2}",
+                    diInfo.Name, diInfo.CreationTime, diInfo.LastAccessTime);
+            }
+            Console.ReadKey();
+        }
+    }
+}
